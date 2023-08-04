9884c9f81c89c6e0700ea01cf82abd2d99d3e130
thuantd8 <thuantd8@fpt.com>
2023-08-05 01:21:14 +0900

    Final Project 3

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=tran12
+export POSTGRES_PASSWORD=Bonghoahong2023
+export POSTGRES_HOST=tran11.csf1yctgtxsq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project3-981421577831
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
